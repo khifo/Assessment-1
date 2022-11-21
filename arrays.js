8ac92bdd6232d4bc22959ebe0f4f18f6f3bdd9f2
khifo <khifo09@yahoo.com>
2022-11-20 23:07:43 -0700

    did not finish problem 4 arrays

--- arrays.js
+++ arrays.js
@@ -33,5 +33,7 @@ let middlesNums = numbers.shift()
 */
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
-
-// CODE HERE
+let answers = []
+for(let i = 0; i < arr.length; i++) {
+  console.log(answers[i]);
+}
